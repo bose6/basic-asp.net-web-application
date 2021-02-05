8216072b63d7f4c7df4d5da6839ccdaa01f09e84
bose6 <77534332+bose6@users.noreply.github.com>
2021-02-05 15:34:45 +0530

    tyhui

---
+++ Source/App_Start/manager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace webdevelopement.App_Start
+{
+    // public class manager
+    //{
+    //}
+}
+public class Setting
+{
+    public string text1 { get; set; }
+    public string text2 { get; set; }
+    public string dFooter { get; set; }
+    public string Header { get; set; }
+    public int visitor =0;
+
+
+}
+public class manager
+{
+
+    public manager()
+    {
+
+
+    }
+
+    public static Setting Loadsetting(string filepath)
+    {
+
+        Setting S = new Setting();
+        try
+        {
+            System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(Setting));
+            System.IO.StreamReader file = new System.IO.StreamReader(filepath);
+            S = (Setting)reader.Deserialize(file);
+            file.Close();
+            return S;
+
+        }
+        catch (Exception)
+        {
+            return S;
+
+        }
+
+    }
+
+    public static void Savesettings(string filepath, Setting Settingobj)
+    {
+        System.Xml.Serialization.XmlSerializer writer = new System.Xml.Serialization.XmlSerializer(typeof(Setting));
+        
+        System.IO.FileStream file = System.IO.File.Create(filepath);
+
+        writer.Serialize(file, Settingobj);
+        file.Close();
+    }
+}
---
+++ Source/home.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace webproject
+{
+    public partial class home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["userid"] == null)
+            {
+                try
+                {
+                    Setting appsetting = (Setting)Application["appsetting"];
+                    text2.Text = appsetting.text2;
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            else
+            {
+                try
+                {
+                    Setting appsetting = (Setting)Application["appsetting"];
+                    text2.Text = appsetting.text1;
+
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+
+        }
+    }
+}
